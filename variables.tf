32676104f62fcb5d42af24dffe9d30669ec2c8cb
akhildanda <akhildanda7@gmail.com>
2024-07-09 10:36:06 +0530

    workstation

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-00e0985cb7de98893"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-057a142abaddec356"
+}
